e512ba2ee43f3deea722e29bd3a6a0f61840eca1
Vivian Lu <85651899+vivianxlu@users.noreply.github.com>
2024-08-26 22:05:46 -0400

    Initial commit

---
+++ src/hw0_main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define ROWS 5
+#define COLS 5
+
+char board[ROWS][COLS] = {
+    {'x', 'x', 'x', '-', 'o'}, 
+    {'o', 'x', 'x', 'o', 'o'}, 
+    {'o', 'x', '-', 'x', 'x'}, 
+    {'o', '-', 'x', 'x', '-'}, 
+    {'x', 'o', 'x', 'o', 'o'}};
+
+
+
+/* Hint:
+
+scanf(" %c", &choice); //this command will erase whitespace in the token selection
+
+*/
+
+int main() {
+    
+    return 0;
+}
+
